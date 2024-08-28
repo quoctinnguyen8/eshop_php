8d8d04e2ec93454a8b4b6622428d9cdae6ea5459
quoctinnguyen8 <nguyenquoctin9x@gmail.com>
2024-08-28 20:40:06 +0700

    danh sách sản phẩm

---
+++ admin/product-list.php
@@ -0,0 +1,61 @@
+<?php include "_header.php";
+
+$sql = "SELECT product.id, product_name, price, 
+            discount_price, image, cate_name
+        FROM product
+        LEFT JOIN product_cate ON product.cate_id = product_cate.id
+        ORDER BY id DESC";
+$data = db_select($sql);
+
+?>
+
+<table>
+    <!-- đặt độ rộng cho từng cột -->
+    <colgroup>
+        <col style="width: 5%;"/>
+        <col style="width: 50%;"/>
+        <col style="width: 10%;"/>
+        <col style="width: 10%;"/>
+        <col style="width: 15%;"/>
+        <col style="width: 10%;"/>
+    </colgroup>
+    <thead>
+        <th>Id</th>
+        <th>Tên sản phẩm</th>
+        <th>Giá</th>
+        <th>Ảnh</th>
+        <th>Danh mục</th>
+        <th><a href="product-add.php">Thêm sản phẩm</a></th>
+    </thead>
+    <tbody>
+        <?php
+        foreach ($data as $value){
+            $id = $value["id"];
+            $name = $value["product_name"];
+            $price = $value["price"];
+            $discount_price = $value["discount_price"];
+            $image = $value["image"];
+            $cate_name = $value["cate_name"];
+        ?>
+            <tr>
+                <td><?= $id ?></td>
+                <td><?= $name ?></td>
+                <td>
+                    <?= $price ?> <br>
+                    <?= $discount_price ?>
+                </td>
+                <td>
+                    <img src="<?= upload($image) ?>" alt="" width="70">
+                </td>
+                <td><?= $cate_name ?></td>
+                <td>
+                    <a href="#?id=<?= $id ?>">Sửa</a>
+                    <a href="#?id=<?= $id ?>"
+                        onclick="return confirm('Xác nhận xóa?')">Xóa</a>
+                </td>
+            </tr>
+        <?php } ?>
+    </tbody>
+</table>
+
+<?php include "_footer.php"; ?>
